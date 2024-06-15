2b95c6a9f1bc59f4db7499c368acfea8aa46214f
Tronilo <luis.tronilo@eu.hsi.local>
2024-06-15 14:50:43 +0200

    Inicio de App. Primera plantilla

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,10 +1,16 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import { PreloadComponent } from './pages/preload/preload.component';
+import { HomeComponent } from './pages/home/home.component';
 
-const routes: Routes = [];
+const routes: Routes = [
+  {path: 'Preload', component: PreloadComponent},
+  {path: 'Home', component: HomeComponent },
+  {path: '**', pathMatch: 'full', redirectTo: 'Home' }
+];
 
 @NgModule({
-  imports: [RouterModule.forRoot(routes)],
+  imports: [RouterModule.forRoot(routes, { useHash: true })],
   exports: [RouterModule]
 })
 export class AppRoutingModule { }
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,10 +3,14 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { PreloadComponent } from './pages/preload/preload.component';
+import { HomeComponent } from './pages/home/home.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    PreloadComponent,
+    HomeComponent
   ],
   imports: [
     BrowserModule,
