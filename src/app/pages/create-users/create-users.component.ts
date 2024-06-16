ea71d14ab65dc7f508fb1cf5cce63a7eda8ac175
Tronilo <luis.tronilo@eu.hsi.local>
2024-06-16 14:15:32 +0200

    Pequeñas logicas implementadas en creacion de usuarios

--- src/app/pages/create-users/create-users.component.ts
+++ src/app/pages/create-users/create-users.component.ts
@@ -1,5 +1,6 @@
 import { Component, ElementRef, Renderer2, ViewChild } from '@angular/core';
 import { IpcService } from '../../services/ipc.service';
+import Swal from 'sweetalert2';
 
 @Component({
   selector: 'app-create-users',
@@ -9,6 +10,26 @@ import { IpcService } from '../../services/ipc.service';
 export class CreateUsersComponent {
 
   @ViewChild('loading') loading: ElementRef;
+  @ViewChild('nombre') nombre: ElementRef;
+  @ViewChild('apellidos') apellidos: ElementRef;
+  @ViewChild('oficina') oficina: ElementRef;
+  @ViewChild('puestoTrabajo') puestoTrabajo: ElementRef;
+  @ViewChild('departamento') departamento: ElementRef;
+  @ViewChild('organizacion') organizacion: ElementRef;
+  @ViewChild('manager') manager: ElementRef;
+  @ViewChild('copia') copia: ElementRef;
+
+  public usersNumb: number = 0;
+  public data: any = {
+    nombre: '',
+    apellidos: '',
+    oficina: '',
+    puestoTrabajo: '',
+    departamento: '',
+    organizacion: '',
+    manager: '',
+    copia: ''
+  }
 
   constructor(
     private renderer: Renderer2,
@@ -18,12 +39,48 @@ export class CreateUsersComponent {
   ngOnInit(): void {
     
   }
-
+  public deleteBorder(field: string): void {
+    if(field === 'nombre') this.renderer.removeClass(this.nombre.nativeElement, 'border__error');
+    if(field === 'apellidos') this.renderer.removeClass(this.apellidos.nativeElement, 'border__error');
+    if(field === 'oficina') this.renderer.removeClass(this.oficina.nativeElement, 'border__error');
+    if(field === 'puestoTrabajo') this.renderer.removeClass(this.puestoTrabajo.nativeElement, 'border__error');
+    if(field === 'departamento') this.renderer.removeClass(this.departamento.nativeElement, 'border__error');
+    if(field === 'organizacion') this.renderer.removeClass(this.organizacion.nativeElement, 'border__error');
+    if(field === 'manager') this.renderer.removeClass(this.manager.nativeElement, 'border__error');
+    if(field === 'copia') this.renderer.removeClass(this.copia.nativeElement, 'border__error');
+  }
   public anadirMas(): void {
-    
+    if(this.data.nombre === '' || 
+      this.data.apellidos === '' ||
+      this.data.oficina === '' ||
+      this.data.puestoTrabajo === '' ||
+      this.data.departamento === '' ||
+      this.data.organizacion === '' ||
+      this.data.manager === '' ||
+      this.data.copia === ''
+    ) {
+      if(this.data.nombre === '') this.renderer.addClass(this.nombre.nativeElement, 'border__error');
+      if(this.data.apellidos === '') this.renderer.addClass(this.apellidos.nativeElement, 'border__error');
+      if(this.data.oficina === '') this.renderer.addClass(this.oficina.nativeElement, 'border__error');
+      if(this.data.puestoTrabajo === '') this.renderer.addClass(this.puestoTrabajo.nativeElement, 'border__error');
+      if(this.data.departamento === '') this.renderer.addClass(this.departamento.nativeElement, 'border__error');
+      if(this.data.organizacion === '') this.renderer.addClass(this.organizacion.nativeElement, 'border__error');
+      if(this.data.manager === '') this.renderer.addClass(this.manager.nativeElement, 'border__error');
+      if(this.data.copia === '') this.renderer.addClass(this.copia.nativeElement, 'border__error');
+      this.alert('Todos los campos son requeridos');
+    }else {
+      console.log(this.data);
+    }
   }
   public crearUsuario(): void {
 
   }
 
+  private alert(text: string): void {
+    Swal.fire({
+      icon: 'info',
+      text,
+      allowOutsideClick: false
+    });
+  }
 }
