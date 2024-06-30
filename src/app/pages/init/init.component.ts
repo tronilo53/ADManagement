d05cc122e2d7ddbfab872a19ca483bd8737f6c0b
Tronilo <Luis.Tronilo@henryschein.es>
2024-06-30 15:57:53 +0200

    Componente Init. comprobando el avatar y el tema elegido. salida en objeto json

--- src/app/pages/init/init.component.ts
+++ src/app/pages/init/init.component.ts
@@ -7,6 +7,9 @@ import { Component, OnInit } from '@angular/core';
 })
 export class InitComponent implements OnInit {
 
+  /**
+   * *Propiedades
+   */
   public items: string[] = ['Batman-256.png', 'Capitan-America-256.png', 'Daredevil-256.png', 'Green-Lantern-256.png', 'Invisible-Woman-256.png', 'Mister-Fantastic-256.png', 'Namor-256.png', 'Silver-Surfer-256.png', 'Superman-256.png', 'the-Thing-256.png'];
   public avatarSelect: string | null = null;
   public themeSelected: string = 'Sweet Honey';
@@ -16,17 +19,37 @@ export class InitComponent implements OnInit {
   ngOnInit(): void {
     
   }
+
+  /**
+   * *Function: Selección de avatar
+   * @param item Avatar elegido
+   */
   public selectAvatar(item: string): void {
+    //Si hay un avatar elegido...
     if(this.avatarSelect) {
+      //Si el avatar elegido es igual al clickeado, el avatar elegido se resetea
       if(this.avatarSelect === item) this.avatarSelect = null;
+      //Si el avatar elegido es distinto al clickeado, el avatar elegido será igual al clickeado.
       else this.avatarSelect = item;
-    }else {
-      this.avatarSelect = item;
-    }
+    //Si no hay un avatar elegido, el avatar elegido será igual al clickeado.
+    }else this.avatarSelect = item;
   }
+
+  /**
+   * *Function: Finalizar Init
+   */
   public finished(): void {
-    
+    const data: any = {
+      avatar: this.avatarSelect,
+      theme: this.themeSelected
+    };
+    console.log(data);
   }
+
+  /**
+   * *Function: Elección del tema del nav
+   * @returns Devuelve el tema del nav (String)
+   */
   public getTypeNav(): string {
     if(this.themeSelected === 'Healthy Sky') return 'bg-primary';
     else if(this.themeSelected === 'Tasty Licorice') return 'bg-danger';
